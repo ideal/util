61477e7b08172e7a3ed43f87591b42dbea82e472
ideal <idealities@gmail.com>
2012-03-31 19:06:04 +0800

    add file

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export KATE_DIR=~/local
+export PATH=$KATE_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$KATE_DIR/lib:$LD_LIBRARY_PATH
+export KDEDIR=$KATE_DIR
+export KDEDIRS=$KDEDIR
+export XDG_DATA_DIRS=$XDG_DATA_DIRS:$KATE_DIR/share
+# update KDE's system configuration cache
+kbuildsycoca4
+# start app
+$@
